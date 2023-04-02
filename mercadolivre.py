cc24220b18f19f2e36c543a53fa695fa4183fdf8
Matheus Schimieguel Silva <mschimieguel@gmail.com>
2023-04-02 08:14:29 -0300

    pesquisa mercado livre

---
+++ mercadolivre.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get("https://www.mercadolivre.com.br/")
+
+caixaPesquisa = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "cb1-edit")))
+
+#elemento.click()
+
+caixaPesquisa.send_keys("Eletrônico")
+caixaPesquisa.send_keys(Keys.RETURN)
+
+
+cookies = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "cookie-consent-banner-opt-out__action--key-accept")))
+cookies.click()
+
+
+time.sleep(300)
+driver.quit()
