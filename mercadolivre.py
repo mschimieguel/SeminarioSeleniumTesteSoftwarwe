fb93c3cbeb0886cb2cd3656ff49c7c1bd071cf87
Matheus Schimieguel Silva <mschimieguel@gmail.com>
2023-04-03 13:26:49 -0300

    Atualizacao Apresentação

--- mercadolivre.py
+++ mercadolivre.py
@@ -8,16 +8,19 @@ import time
 driver = webdriver.Chrome('chromedriver.exe')
 driver.get("https://www.mercadolivre.com.br/")
 
-caixaPesquisa = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "cb1-edit")))
-
-#elemento.click()
+caixaPesquisa = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located(
+        (By.ID, "cb1-edit")
+    )
+)
 
 caixaPesquisa.send_keys("Eletrônico")
 caixaPesquisa.send_keys(Keys.RETURN)
 
-
 time.sleep(9)
+
 #Clicando para aceitar os termos de cookie
+
 cookies = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "cookie-consent-banner-opt-out__action--key-accept")))
 cookies.click()
 
